508434646f9f2f4f13b519cd9aed36dfc74f980b
Marek Hulan <ares@igloonet.cz>
2011-06-09 11:29:26 +0200

    Fixed multipart sending

--- app/models/wiki_digest_mailer.rb
+++ app/models/wiki_digest_mailer.rb
@@ -5,4 +5,17 @@ class WikiDigestMailer < ActionMailer::Base
     body :data => data
     render_multipart('wiki_digest', body)
   end
+  
+  private
+  
+  def render_multipart(method_name, body)
+    if Setting.plain_text_mail?
+      content_type "text/plain"
+      body render(:file => "#{method_name}.text.plain.rhtml", :body => body, :layout => 'mailer.text.plain.erb')
+    else
+      content_type "multipart/alternative"
+      part :content_type => "text/plain", :body => render(:file => "#{method_name}.text.plain.rhtml", :body => body, :layout => 'mailer.text.plain.erb')
+      part :content_type => "text/html", :body => render_message("#{method_name}.text.html.rhtml", body)
+    end
+  end
 end
